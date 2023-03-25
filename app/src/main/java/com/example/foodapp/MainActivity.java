1101c79caded9ef312e9e0ec53b231f1b3a11c4e
MonishkaG <gautam.monishka@gmail.com>
2023-03-25 03:39:28 -0700

    Done: button click and next page

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppCompatImageButton heartBtn;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        heartBtn = (AppCompatImageButton) findViewById(R.id.heart_img_button);
+        heartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openList();
+            }
+        });
+    }
+    public void openList(){
+        Intent intent = new Intent(this, wishListActivity.class);
+        startActivity(intent);
     }
 }
